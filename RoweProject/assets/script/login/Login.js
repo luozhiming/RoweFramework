4fd0f5afb25f643f1310ad239f67d57f5f539f4a
Chester Rowe <luozhiming2009@gmail.com>
2019-01-15 15:49:15 +0800

    Update Login.js

--- RoweProject/assets/script/login/Login.js
+++ RoweProject/assets/script/login/Login.js
@@ -2,10 +2,19 @@ cc.Class({
     extends: cc.Component,
 
     start () {
+        this.GE = require("GE").getData();
+        this.GE.event.register(1, "LAYER_LOAD_OVER", this.eventCB.bind(this));
+    },
 
+    onDestroy() {
+        require("GE").getData().unregister(1, "LAYER_LOAD_OVER");
     },
 
     onLoginClick() {
-        var GE = require("GE").getData();
+        this.GE.event.dispatch("LAYER_LOAD_OVER");
+    },
+
+    eventCB() {
+        this.GE.ui.showTip("TEST");
     }
 });
